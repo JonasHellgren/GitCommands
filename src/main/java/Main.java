dd11a7fa7c9f78d7e2f30b4f86ab246f9cd171ae
Hellgren Jonas <jonas.hellgren@volvo.com>
2021-06-16 11:35:42 +0200

    Old Jonas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        String joe = String.format("%s = %d", "Jonas", 35);
+        String joe = String.format("%s = %d", "Jonas", 135);
         String jim = String.format("%s = %d", "Katja", 33);
         StringJoiner output = new StringJoiner(",", "[", "]");
         output.add(joe);
